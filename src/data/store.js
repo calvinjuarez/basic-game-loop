36333b6f77cba3c79e9e44a9e4d899fc25e2c7ca
Calvin Juárez <calvin.juarez@gmail.com>
2023-11-15 15:22:24 -0700

    store – create

---
+++ src/data/store.js
@@ -0,0 +1,22 @@
+import { reactive } from 'vue'
+
+
+export class StoreTypeError extends TypeError {}
+
+export default reactive({
+	display: null,
+	displayHeight: 0,
+	displayWidth: 0,
+	x: 0,
+	y: 0,
+
+	setDisplay(canvas) {
+		if (! canvas instanceof HTMLCanvasElement)
+			throw new StoreTypeError(`setDisplay() requires 'canvas' to be an
+				HTMLCanvasElement instance.`)
+
+		this.display = canvas.getContext('2d');
+		this.displayWidth = canvas.width;
+		this.displayHeight = canvas.height;
+	},
+});
