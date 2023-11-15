d670c021911163f54c12ee7368a018aa5b737be5
Calvin Juárez <calvin.juarez@gmail.com>
2023-11-15 13:08:05 -0700

    Game – add canvas

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 import Clock from '@/game/Clock.js';
 
@@ -17,10 +17,32 @@ window.clock = clock;
 </script>
 
 <template>
-	<dl class="dl-cols">
-		<dt>fps:</dt>
-		<dd><output>{{ fps }}</output></dd>
-	</dl>
+	<div class="game">
+		<canvas
+			class="game-display"
+			id="game-display"
+			width="1600"
+			height="900"
+		></canvas>
+		<aside class="game-dev-info">
+			<h5>Dev Info</h5>
+			<dl class="dl-cols">
+				<dt>fps:</dt>
+				<dd><output>{{ fps }}</output></dd>
+			</dl>
+		</aside>
+	</div>
 </template>
 
-<style></style>
+<style>
+.game {
+	display: flex;
+	gap: var(--spacer-2);
+}
+.game-display {
+	outline: 1px solid #b9b9b9; /* don't wanna add space to the box model */
+	width: 100%;
+	max-width: 400px;
+}
+.game-dev-info {}
+</style>
