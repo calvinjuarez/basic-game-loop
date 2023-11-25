b4c7e63f05096b65fdc181f8889462d9ba60af0c
Calvin Juárez <calvin.juarez@gmail.com>
2023-11-25 15:54:13 -0700

    utils/number – create proper modulo

--- src/util/number.js
+++ src/util/number.js
@@ -7,3 +7,13 @@
 export function clamp(value, lower, upper) {
 	return Math.max(Math.min(value, upper), lower);
 }
+
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @returns {number} A proper modulo of number by divisor (rather than a remainder).
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder#description
+ */
+export function modulo(dividend, divisor) {
+	return ((dividend % divisor) + divisor) % divisor;
+}
