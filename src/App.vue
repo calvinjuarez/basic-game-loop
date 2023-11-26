a441363f6cbdafa7c13d44f8c260ae3fa874673c
Calvin Juárez <calvin.juarez@gmail.com>
2023-11-25 18:47:24 -0700

    index – update title and heading

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ provide('store', store);
 
 <template>
 	<header>
-		<h1 class="display-1">Basic Game Loop</h1>
+		<h1 class="display-1">Browser Game</h1>
 		<p class="lead">A sandbox.</p>
 	</header>
 
