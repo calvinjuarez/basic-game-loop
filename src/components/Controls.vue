4955faa4d7e0ffd1ea7b6e82e573897beee2ffeb
Calvin Juárez <calvin.juarez@gmail.com>
2023-12-21 19:28:33 -0700

    Game, Controls – remove listeners before unmount

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, onMounted } from 'vue';
+import { inject, onBeforeUnmount, onMounted } from 'vue';
 
 import Joystick from '@/components/Joystick.vue';
 
@@ -10,28 +10,38 @@ const Key = { 87: 'w', 65: 'a', 83: 's', 68: 'd', 16: 'shift' };
 const KEYS = Object.keys(Key).map(key => +key);
 
 
+const _onKeyDown = e => {
+	if (store.input.hasKey)
+		e.preventDefault();
+
+	if (e.altKey || e.ctrlKey || e.metaKey) return;
+	if (! KEYS.includes(e.keyCode)) return;
+
+	store.input.key[Key[e.keyCode]] = true;
+};
+const _onKeyUp = e => {
+	if (store.input.hasKey)
+		e.preventDefault();
+
+	if (! KEYS.includes(e.keyCode)) return;
+
+	store.input.key[Key[e.keyCode]] = false;
+};
+const _onBeforeUnload = e => {
+	if (store.input.hasKey)
+		e.preventDefault();
+};
+
+
 onMounted(() => {
-	window.addEventListener('keydown', e => {
-		if (store.input.hasKey)
-			e.preventDefault();
-
-		if (e.altKey || e.ctrlKey || e.metaKey) return;
-		if (! KEYS.includes(e.keyCode)) return;
-
-		store.input.key[Key[e.keyCode]] = true;
-	}, false);
-	window.addEventListener('keyup', e => {
-		if (store.input.hasKey)
-			e.preventDefault();
-
-		if (! KEYS.includes(e.keyCode)) return;
-
-		store.input.key[Key[e.keyCode]] = false;
-	}, false);
-	window.addEventListener('beforeunload', e => {
-		if (store.input.hasKey)
-			e.preventDefault();
-	});
+	window.addEventListener('keydown', _onKeyDown, false);
+	window.addEventListener('keyup', _onKeyUp, false);
+	window.addEventListener('beforeunload', _onBeforeUnload);
+});
+onBeforeUnmount(() => {
+	window.removeEventListener('keydown', _onKeyDown, false);
+	window.removeEventListener('keyup', _onKeyUp, false);
+	window.removeEventListener('beforeunload', _onBeforeUnload);
 });
 </script>
 
